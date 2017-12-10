54121f54ce627c0eda7fd82617b385d59fe93568
asifexpert2 <asifexpert2@gmail.com>
2017-12-10 19:24:19 +0400

    fixed

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,4 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
-Route::get('/customers',['as'=>'customerApi','uses'=>'reportingController@customer_api'])
+Route::get('/customers',['as'=>'customerApi','uses'=>'reportingController@customer_api']);
