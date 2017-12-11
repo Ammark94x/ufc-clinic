4bf6ac237918550df00b7f4a362b7c386eb32c3d
Ammar <ammarkhan94@outlook.com>
2017-12-10 19:19:47 -1200

    test
    push bby push

--- app/Http/Controllers/reportingController.php
+++ app/Http/Controllers/reportingController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class reportingController extends Controller
 {
-    //
+    public function customers()
+    {
+    	$customers = User::whereNull('role')->get();
+    	return view('admin.customer_reports',['data'=>$customers]);
+    }
+    
 }
